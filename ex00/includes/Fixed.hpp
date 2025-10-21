2b706ea9d74c1e26e5d5fbcda7d778dfe4ff65ef
CRaph0705 <raphael.cochrane94@gmail.com>
2025-10-21 13:42:22 +0200

    before reboot bocal

--- ex00/includes/Fixed.hpp
+++ ex00/includes/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: rcochran <rcochran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 10:56:40 by rcochran          #+#    #+#             */
-/*   Updated: 2025/10/20 17:50:34 by rcochran         ###   ########.fr       */
+/*   Updated: 2025/10/21 13:38:30 by rcochran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,9 @@ public :
 	Fixed(void);
 	//cpy construct
 	Fixed(const Fixed&);
+	//operateur d'affectation
+	// A::operator=(const A&)
+	
 	//destructor
 	~Fixed(void);
 };
