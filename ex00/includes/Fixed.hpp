e2fdaba24420e35aa5d29e3eb47f1d677df07223
CRaph0705 <raphael.cochrane94@gmail.com>
2025-10-21 16:26:42 +0200

    ex00 added

--- ex00/includes/Fixed.hpp
+++ ex00/includes/Fixed.hpp
@@ -6,17 +6,18 @@
 /*   By: rcochran <rcochran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 10:56:40 by rcochran          #+#    #+#             */
-/*   Updated: 2025/10/21 15:21:33 by rcochran         ###   ########.fr       */
+/*   Updated: 2025/10/21 16:11:29 by rcochran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+#include <iostream>
 class Fixed {
-
+// https://en.wikipedia.org/wiki/Fixed-point_arithmetic
 private :
-	int	part;
-	const static int fractionnal;
+	int	_part;
+	const static int _fractionnal_part;
 
 public :
 	//default construct
--- ex00/src/Fixed.cpp
+++ ex00/src/Fixed.cpp
@@ -6,34 +6,49 @@
 /*   By: rcochran <rcochran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 10:56:43 by rcochran          #+#    #+#             */
-/*   Updated: 2025/10/21 15:18:27 by rcochran         ###   ########.fr       */
+/*   Updated: 2025/10/21 16:24:47 by rcochran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include <iostream>
 
+const int Fixed::_fractionnal_part = 8;
+
 Fixed::Fixed(void)
 {
 	std::cout << "Default constructor called." << std::endl;
-	this->part = 0;
+	this->_part = 0;
 	return ;
 }
 
 Fixed::Fixed(const Fixed &f)
 {
 	std::cout << "Copy constructor called." << std::endl;
+	*this = f;
 	return ;
 }
 
 Fixed	&Fixed::operator=(const Fixed& f)
 {
-	std::cout << "Assignment operator called." << std::endl;
-	return ;
+	std::cout << "Copy assignment operator called." << std::endl;
+	this->_part = f.getRawBits();
+	return (*this);
 }
 
 Fixed::~Fixed(void)
 {
 	std::cout << "Destructor called." << std::endl;
 	return ;
+}
+int	Fixed::getRawBits( void ) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_part);
+}
+
+void	Fixed::setRawBits( int const raw )
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_part = raw;
 }
